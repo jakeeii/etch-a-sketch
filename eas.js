6e60ab48fa2931e308dacf5898390fa4c8a19698
Jake <jakevisc@gmail.com>
2022-04-01 11:05:59 -0230

    Add mouseEnter event to color grid tiles
    I have added a mouseEnter event to each tile by creating a unique ID
    ("tile" + i) during the createGrid method. The event then colors in
    the whole tile black when the mouse enters the tile.

--- eas.js
+++ eas.js
@@ -6,7 +6,12 @@ function createGrid(){
   for (let i = 0; i < 16 * 16; i++){
     let grid = document.getElementById('grid')
     let newDivSqr = document.createElement('div')
+    newDivSqr.id = "tile" + i
     grid.appendChild(newDivSqr)
+    let tile = document.getElementById(newDivSqr.id)
+    tile.addEventListener('mouseenter', e => {
+      tile.style.backgroundColor = 'black'
+    })
   }
 
 }
